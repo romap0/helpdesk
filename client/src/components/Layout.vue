7c5617a304cf8dd03518945cc68a505c4d0e9c34
Roman Pavlov <pavlov@pqlab.ru>
2018-04-16 17:26:56 +0300

    Set layout

---
+++ client/src/components/Layout.vue
@@ -0,0 +1,145 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      fixed
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+      v-model="drawer"
+    >
+      <v-list dense>
+        <template v-for="item in items">
+          <v-layout
+            row
+            v-if="item.heading"
+            align-center
+            :key="item.heading"
+          >
+            <v-flex xs6>
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-flex>
+            <v-flex
+              xs6
+              class="text-xs-center">
+              <a
+                href="#!"
+                class="body-2 black--text">EDIT</a>
+            </v-flex>
+          </v-layout>
+          <v-list-group
+            v-else-if="item.children"
+            v-model="item.model"
+            :key="item.text"
+            :prepend-icon="item.model ? item.icon : item['icon-alt']"
+            append-icon=""
+          >
+            <v-list-tile slot="activator">
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ item.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile
+              v-for="(child, i) in item.children"
+              :key="i"
+              :to="child.to"
+            >
+              <v-list-tile-action v-if="child.icon">
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ child.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list-group>
+          <v-list-tile
+            v-else
+            :key="item.text"
+            :to="item.to"
+          >
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      color="blue darken-3"
+      dark
+      app
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      fixed
+    >
+      <v-toolbar-title
+        style="width: 300px"
+        class="ml-0 pl-3">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"/>
+        <span class="hidden-sm-and-down">МедТехРейс</span>
+      </v-toolbar-title>
+
+    </v-toolbar>
+    <v-content>
+      <v-container
+        fluid
+        fill-height>
+        <v-layout>
+          <router-view/>
+        </v-layout>
+      </v-container>
+    </v-content>
+
+    <!-- Sync dialog -->
+    <v-dialog
+      persistent
+      v-model="showSyncModal"
+      max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span>Загрузка данных</span>
+        </v-card-title>
+        <div class="center">
+          <v-progress-circular
+            indeterminate
+            :size="70"
+            :width="7"
+            color="primary"/>
+        </div>
+      </v-card>
+    </v-dialog>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+    items: [
+      { icon: 'view_list', text: 'Заявки', to: '/tickets' },
+      { icon: 'supervisor_account', text: 'Клиенты', to: '/clients' },
+      { icon: 'computer', text: 'Оборудование', to: '/equipment' }
+    ]
+  }),
+  computed: {
+  }
+}
+</script>
+
+<style>
+.progress-circular {
+  margin: 2rem
+}
+
+.center {
+  text-align: center
+}
+</style>
