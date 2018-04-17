f22abd0a73bf0c6a178ec6f11e12d07ce7981e5f
Roman Pavlov <pavlov@pqlab.ru>
2018-04-17 19:09:24 +0300

    Add ticket status in constructor

--- server/src/enums.js
+++ server/src/enums.js
@@ -2,3 +2,10 @@ export const StoreName = {
   Tickets: 'tickets',
   Users: 'users'
 }
+
+export const TicketStatus = {
+  New: 'Новый',
+  Active: 'Активный',
+  Resolved: 'Решённый',
+  Closed: 'Закрытый'
+}
--- server/src/models/ticket.js
+++ server/src/models/ticket.js
@@ -1,9 +1,11 @@
 import { ObjectId } from 'mongodb'
+import { TicketStatus } from '../enums'
 
 export class Ticket {
   constructor (userId, title) {
     this.userId = ObjectId(userId)
     this.title = title
     this.date = new Date()
+    this.status = TicketStatus.New
   }
 }
